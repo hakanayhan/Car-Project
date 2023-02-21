e3e351dbe783c84ab31f1002e152aa873b415b54
Hakan Ayhan <hakanayhnn@gmail.com>
2023-02-21 17:02:48 +0300

    +add: basic camera controller

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CarProject.Camera
+
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CameraSettings _cameraSettings;
+        [SerializeField] private Transform _targetTransform;
+        [SerializeField] private Transform _cameraTransform;
+        private void Update()
+        {
+            CameraRotationFollow();
+            CameraMovementFollow();
+        }
+
+        private void CameraRotationFollow()
+        {
+            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, Quaternion.LookRotation(_targetTransform.forward), Time.deltaTime * _cameraSettings.RotationLerpSpeed);
+        }
+
+        private void CameraMovementFollow()
+        {
+            //Vector3 offset = (_cameraTransform.right * _cameraSettings.PositionOffset.x) + (_cameraTransform.up * _cameraSettings.PositionOffset.y) + (_cameraTransform.forward * _cameraSettings.PositionOffset.z);
+            //_cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _targetTransform.position + offset, Time.deltaTime * _cameraSettings.PositionLerp);
+            _cameraTransform.localPosition = _cameraSettings.PositionOffset;
+        }
+    }
+}
